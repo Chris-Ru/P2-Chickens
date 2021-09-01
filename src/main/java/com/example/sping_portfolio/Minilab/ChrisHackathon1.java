11f355fd97dbf62812c80d368a93d9e471b68a8c
Chris-Ru <72892271+Chris-Ru@users.noreply.github.com>
2021-09-01 10:05:15 -0700

    Update individual hackathon and make minilab folder

---
+++ src/main/java/com/example/sping_portfolio/Minilab/ChrisHackathon1.java
@@ -0,0 +1,23 @@
+package com.example.sping_portfolio.Minilab;
+
+
+public class ChrisHackathon1 {
+
+    public static void main(String[] args) {
+        double Test1 = 65.8;
+        double Test2 = 83.5;
+        double Test3 = 92.6;
+        double Test4 = 42.7;
+        double Test5 = 100;
+
+        double avg12 = (Test1 + Test2)/2;
+        double avg34 = (Test3 + Test4)/2;
+
+        System.out.println("avg12: "+avg12);
+        System.out.println("avg34: "+avg34);
+        System.out.print("Total: "+((avg12)+(avg34)+(Test5))/5);
+
+
+    }
+
+}
