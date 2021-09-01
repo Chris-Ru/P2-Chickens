d6085f8bb94a41c0a9d0033491f91fec90c4a406
shekar <shekar.km26@gmail.com>
2021-09-01 10:16:00 -0700

    created hackathon java file and inserted code

---
+++ src/main/java/com/example/sping_portfolio/Minilab/Hackathon1/shekar_hackathon_1.java
@@ -0,0 +1,21 @@
+package com.example.sping_portfolio.Minilab;
+
+
+public class shekar_hackathon_1 {
+
+    public static void main(String[] args) {
+        double Test1 = 67.8;
+        double Test2 = 73.5;
+        double Test3 = 81.6;
+        double Test4 = 66.1;
+        double Test5 = 95;
+
+
+
+        System.out.println("hello test");
+
+
+    }
+
+}
+
