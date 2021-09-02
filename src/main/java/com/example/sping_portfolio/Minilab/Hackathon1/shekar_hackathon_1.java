907efa0f1ccab7ee107f6df6f8117aecef649c6f
shekar <shekar.km26@gmail.com>
2021-09-02 10:05:24 -0700

    created hackathon java file and inserted code

--- src/main/java/com/example/sping_portfolio/Minilab/Hackathon1/shekar_hackathon_1.java
+++ src/main/java/com/example/sping_portfolio/Minilab/Hackathon1/shekar_hackathon_1.java
@@ -9,10 +9,16 @@ public class shekar_hackathon_1 {
         double Test3 = 81.6;
         double Test4 = 66.1;
         double Test5 = 95;
-
+        double avg12 = (Test1+Test2) / 2;
+        double avg34 = (Test3+Test4) / 2;
+        double finalavg = (avg12 + avg34 + Test5) / 3;
 
 
         System.out.println("hello test");
+        System.out.println("average of test 1 and 2 is " + avg12);
+        System.out.println("average of test 3 and 4 is " + avg34);
+        System.out.println("Final average is " + finalavg);
+
 
 
     }
