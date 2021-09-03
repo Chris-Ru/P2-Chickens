5cb8857bcc3808e816139d56c36080b8b8e96a32
dylanroman <dylanroman143@gmail.com>
2021-09-03 10:31:16 -0700

    Added Dylan weekly lab

---
+++ src/main/java/com/example/sping_portfolio/Minilab/Hackathon1/DylanHackathon1.java
@@ -0,0 +1,21 @@
+package com.example.sping_portfolio.Minilab.Hackathon1;
+
+public class DylanHackathon1 {
+
+    public static void main(String[] args) {
+        double test1 = 70;
+        double test2 = 80;
+        double test3 = 90;
+        double test4 = 100;
+        double test5 = 95;
+
+        double gp1avg = (test1 + test2)/2;
+        double gp2avg = (test3 + test4)/2;
+        double totalavg = (test1 + test2 + test3 + test4 + test5)/5;
+
+        System.out.println("Grading Period 1 Average" + gp1avg);
+        System.out.println("Grading Period 2 Average" + gp2avg);
+        System.out.print("Total Average" + totalavg);
+    }
+
+}
