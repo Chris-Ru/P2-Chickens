615ca3d5cf3843cc0a11571cf8472da79ea8d66a
Chris-Ru <72892271+Chris-Ru@users.noreply.github.com>
2021-09-08 09:48:38 -0700

    fix individual greet page for chris.html

--- src/main/java/com/example/sping_portfolio/individual/Chris.java
+++ src/main/java/com/example/sping_portfolio/individual/Chris.java
@@ -1,14 +1,19 @@
 package com.example.sping_portfolio.individual;
+        /* MVC code that shows defining a simple Model, calling View, and this file serving as Controller
+         * Web Content with Spring MVCSpring Example: https://spring.io/guides/gs/serving-web-con
+         */
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+        import org.springframework.stereotype.Controller;
+        import org.springframework.ui.Model;
+        import org.springframework.web.bind.annotation.GetMapping;
+        import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller  // HTTP requests are handled as a controller, using the @Controller annotation
 public class Chris {
-    @GetMapping("/chris")
-    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
-    public String chris() {
+    @GetMapping("/chris")    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
+    public String chris(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        // @RequestParam handles required and default values, name and model are class variables, model looking like JSON
+        model.addAttribute("name", name); // MODEL is passed to html
         return "individual/chris"; // returns HTML VIEW (greeting)
     }
 }
-
