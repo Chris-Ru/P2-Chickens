b9fcfb1f8a8f1ed24dfed7a59f1d5adfcd5bceee
shekar <shekar.km26@gmail.com>
2021-09-09 10:09:36 -0700

    finished minilab

---
+++ src/main/java/com/example/sping_portfolio/Minilab/Minilab2/Workout.java
@@ -0,0 +1,55 @@
+package com.example.sping_portfolio.Minilab.Minilab2;
+
+public class Workout {
+    private static final int oil = 0;
+    //protected data
+    private String muscle;
+    private int reps;
+    private int weight;
+
+    //constructor
+    public Workout(String muscle, int reps, int weight)
+    {
+        this.muscle = muscle;
+        this.reps = reps;
+        this.weight = weight;
+    }
+
+    //get protected data
+
+    public String getMuscle()
+    {
+        return muscle;
+    }
+
+    public int getReps()
+    {
+        return reps;
+    }
+
+    public int getWeight()
+    {
+        return weight;
+    }
+
+    //method
+    public void endurance()
+    {
+        if (this.reps >= 30)
+        {
+            System.out.println("This set is meant for endurance and hypertrophy");
+        }
+    }
+
+
+    //call class
+    public static void main(String[] args)
+    {
+        Workout myWorkout = new Workout("Chest", 12, 225);
+        System.out.println("Muscle: " + myWorkout.getMuscle());
+        System.out.println("Reps: " + myWorkout.getReps());
+        System.out.println("Weight: " + myWorkout.getWeight());
+        myWorkout.endurance();
+
+    }
+}
