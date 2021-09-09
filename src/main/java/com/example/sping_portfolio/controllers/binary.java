fe2df7b12671cd761f5e828cbba2f9d920e70328
Chris-Ru <72892271+Chris-Ru@users.noreply.github.com>
2021-09-09 10:30:47 -0700

    binary minilab layout

---
+++ src/main/java/com/example/sping_portfolio/controllers/binary.java
@@ -0,0 +1,17 @@
+package com.example.sping_portfolio.controllers;
+/* MVC code that shows defining a simple Model, calling View, and this file serving as Controller
+ * Web Content with Spring MVCSpring Example: https://spring.io/guides/gs/serving-web-con
+ */
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller  // HTTP requests are handled as a controller, using the @Controller annotation
+public class binary {
+    @GetMapping("/binary")    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
+    public String binary() {
+        return "minilab2"; // returns HTML VIEW (greeting)
+    }
+}
