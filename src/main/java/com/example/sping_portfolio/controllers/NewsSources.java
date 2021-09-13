cb20245b2c04cab3de4a9269c94a56948ed0f184
shekar <shekar.km26@gmail.com>
2021-09-12 20:23:49 -0700

    Created News Sources Page

---
+++ src/main/java/com/example/sping_portfolio/controllers/NewsSources.java
@@ -0,0 +1,13 @@
+package com.example.sping_portfolio.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller  // HTTP requests are handled as a controller, using the @Controller annotation
+public class NewsSources {
+    @GetMapping("/NewsSource")
+    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
+    public String NewsSource() {
+        return "NewsSource"; // returns HTML VIEW (greeting)
+    }
+}
