683ac55692546d5318c99cb2e3554640a661f17e
dylanroman <dylanroman143@gmail.com>
2021-09-14 10:31:39 -0700

    fixed gridtest

--- src/main/java/com/example/sping_portfolio/controllers/gridtest.java
+++ src/main/java/com/example/sping_portfolio/controllers/gridtest.java
@@ -9,8 +9,23 @@ import com.example.sping_portfolio.functions.news;
 public class gridtest {
     @GetMapping("/gridtest")
     public String gridtest(Model model){
+        //source 1
         news source1 = new news("source 1", 5, 20);
-        model.addAttribute("source1", source1);
+        model.addAttribute("source1name", source1.getArticle_name());
+        model.addAttribute("source1rating", source1.getRating());
+        model.addAttribute("source1total", source1.getReview_total());
+        //source 2
+        news source2 = new news("source 2", 4, 103);
+        model.addAttribute("source2name", source2.getArticle_name());
+        model.addAttribute("source2rating", source2.getRating());
+        model.addAttribute("source2total", source2.getReview_total());
+        //source 3
+        news source3 = new news("source 3", 5, 17);
+        model.addAttribute("source3name", source3.getArticle_name());
+        model.addAttribute("source3rating", source3.getRating());
+        model.addAttribute("source3total", source3.getReview_total());
+
+        //render template
         return "gridtest";
     }
 }
