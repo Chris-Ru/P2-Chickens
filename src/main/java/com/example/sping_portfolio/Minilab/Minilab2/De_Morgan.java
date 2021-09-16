c1972b041446ee9306d962ad390029745a0bd258
Chris-Ru <72892271+Chris-Ru@users.noreply.github.com>
2021-09-15 23:27:32 -0700

    Add new TPT3 to minilab

---
+++ src/main/java/com/example/sping_portfolio/Minilab/Minilab2/De_Morgan.java
@@ -0,0 +1,19 @@
+package com.example.sping_portfolio.Minilab.Minilab2;
+
+class De_Morgan {
+    public static void main(String[] args) {
+        boolean oversixfoot = false;
+        boolean looksfit = true;
+
+        if (oversixfoot && looksfit){
+            System.out.println("You are like Allen (100% chance)");
+        }
+        else if (!(oversixfoot || !looksfit)){
+            System.out.println("You are Charlie (0% chance)");
+        }
+        else if (oversixfoot || looksfit){
+            System.out.println("You have a 50% chance");
+        }
+    }
+
+}
