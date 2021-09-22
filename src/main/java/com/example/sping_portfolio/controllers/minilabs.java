ec234dd4ddf9b620d9040d14295096d2c0d27169
Chris-Ru <72892271+Chris-Ru@users.noreply.github.com>
2021-09-22 00:53:20 -0700

    about finish fibonacci sequence with palindrome learn how to add java code into printed statement on webpage

--- src/main/java/com/example/sping_portfolio/controllers/minilabs.java
+++ src/main/java/com/example/sping_portfolio/controllers/minilabs.java
@@ -10,21 +10,24 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller  // HTTP requests are handled as a controller, using the @Controller annotation
 public class minilabs {
-    @GetMapping("/minilab1")    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
-    public String chris(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+    @GetMapping("/minilab1")
+    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
+    public String chris(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
         // @RequestParam handles required and default values, name and model are class variables, model looking like JSON
         model.addAttribute("name", name); // MODEL is passed to html
         return "Minilab/minilab1"; // returns HTML VIEW (greeting)
     }
 
-    @GetMapping("/binary")    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
-    public String Binary(@RequestParam(name="bits", required=false, defaultValue="8") int bits, Model model) {
+    @GetMapping("/binary")
+    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
+    public String Binary(@RequestParam(name = "bits", required = false, defaultValue = "8") int bits, Model model) {
         model.addAttribute("bits", bits); // MODEL is passed to html
         return "Minilab/minilab2"; // returns HTML VIEW ()
     }
 
-    @GetMapping("/fib")    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
-    public String Fib(@RequestParam(name="word", required=false, defaultValue="Word") String word, Model model) {
+    @GetMapping("/fib")
+    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
+    public String Fib(@RequestParam(name = "word", required = false, defaultValue = "Word") String word, Model model) {
         model.addAttribute("word", word); // MODEL is passed to html
         return "Minilab/minilab3"; // returns HTML VIEW ()
     }
