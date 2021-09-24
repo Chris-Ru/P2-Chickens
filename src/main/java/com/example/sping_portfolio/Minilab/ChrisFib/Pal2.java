be534421497c7d2f5a5597b0595e81eda651db2b
Chris-Ru <72892271+Chris-Ru@users.noreply.github.com>
2021-09-24 10:27:16 -0700

    add files and rename String functions

---
+++ src/main/java/com/example/sping_portfolio/Minilab/ChrisFib/Pal2.java
@@ -0,0 +1,42 @@
+package com.example.sping_portfolio.Minilab.ChrisFib;
+
+// Java implementation of the approach
+public class Pal2 {
+
+    // Function that returns true if
+    // str is a palindrome
+    static boolean isPalindrome(String str)
+    {
+
+        // Pointers pointing to the beginning
+        // and the end of the string
+        int i = 0, j = str.length() - 1;
+
+        // While there are characters to compare
+        while (i < j) {
+
+            // If there is a mismatch
+            if (str.charAt(i) != str.charAt(j))
+                return false;
+
+            // Increment first pointer and
+            // decrement the other
+            i++;
+            j--;
+        }
+
+        // Given string is a palindrome
+        return true;
+    }
+
+    // Driver code
+    public static void main(String[] args)
+    {
+        String str = "geeks";
+
+        if (isPalindrome(str))
+            System.out.print("Yes");
+        else
+            System.out.print("No");
+    }
+}
--- src/main/java/com/example/sping_portfolio/Minilab/ChrisFib/TPT5.java
+++ src/main/java/com/example/sping_portfolio/Minilab/ChrisFib/TPT5.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class TPT5 {
     @GetMapping("/palindrome1")
     // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
-    public String Pal(@RequestParam(name = "word", required = false, defaultValue = "") String word, Model model) {
+    public String Pal1(@RequestParam(name = "word", required = false, defaultValue = "") String word, Model model) {
         if (Pal1.checkPalindrome(word)) {
             System.out.println(word + " palindrome");
             // uses ${message} from inputted date on Pal1.html and prints the word with " is a palindrome"
@@ -19,5 +19,21 @@ public class TPT5 {
         }
 
         return "Minilab/ChrisTPT5/pal1"; // returns HTML VIEW ()
+    }
+
+    @GetMapping("/palindrome2")
+    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
+    public String Pal2(@RequestParam(name = "word", required = false, defaultValue = "") String word, Model model) {
+        if (Pal1.checkPalindrome(word)) {
+            System.out.println(word + " palindrome");
+            // uses ${message} from inputted date on Pal1.html and prints the word with " is a palindrome"
+            model.addAttribute("message", word + " is a palindrome"); // MODEL is passed to html
+        } else {
+            System.out.println(word + " not a palindrome");
+            // uses ${message} from inputted data on Pal1.html and prints the word with " is not a palindrome"
+            model.addAttribute("message", word + " is not a palindrome");
+        }
+
+        return "Minilab/ChrisTPT5/pal2"; // returns HTML VIEW ()
     }
 }
