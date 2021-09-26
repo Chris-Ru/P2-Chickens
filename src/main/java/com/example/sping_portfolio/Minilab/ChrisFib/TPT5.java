c71c9828247047abc26617edc7a1f1aa405d6d94
Chris-Ru <72892271+Chris-Ru@users.noreply.github.com>
2021-09-25 23:51:45 -0700

    half of Palindrome algorithm done

--- src/main/java/com/example/sping_portfolio/Minilab/ChrisFib/TPT5.java
+++ src/main/java/com/example/sping_portfolio/Minilab/ChrisFib/TPT5.java
@@ -43,4 +43,38 @@ public class TPT5 {
 
         return "Minilab/ChrisTPT5/Pal2"; // returns HTML VIEW ()
     }
+
+    @GetMapping("/palindrome3")
+    //   algorithm
+    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
+    public String Pal3(@RequestParam(name = "word", required = false, defaultValue = "") String word, Model model) {
+        if (Pal2.isPalindrome(word)) {
+            System.out.println(word + " palindrome");
+            // uses ${message} from inputted date on Pal1.html and prints the word with " is a palindrome"
+            model.addAttribute("message", word + " is a palindrome"); // MODEL is passed to html
+        } else {
+            System.out.println(word + " not a palindrome");
+            // uses ${message} from inputted data on Pal1.html and prints the word with " is not a palindrome"
+            model.addAttribute("message", word + " is not a palindrome");
+        }
+
+        return "Minilab/ChrisTPT5/Pal3"; // returns HTML VIEW ()
+    }
+
+    @GetMapping("/palindrome4")
+    //   algorithm
+    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
+    public String Pal4(@RequestParam(name = "word", required = false, defaultValue = "") String word, Model model) {
+        if (Pal2.isPalindrome(word)) {
+            System.out.println(word + " palindrome");
+            // uses ${message} from inputted date on Pal1.html and prints the word with " is a palindrome"
+            model.addAttribute("message", word + " is a palindrome"); // MODEL is passed to html
+        } else {
+            System.out.println(word + " not a palindrome");
+            // uses ${message} from inputted data on Pal1.html and prints the word with " is not a palindrome"
+            model.addAttribute("message", word + " is not a palindrome");
+        }
+
+        return "Minilab/ChrisTPT5/Pal4"; // returns HTML VIEW ()
+    }
 }
