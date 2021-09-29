a3ed63364e310184042a92036ad43ef055922244
Chris-Ru <72892271+Chris-Ru@users.noreply.github.com>
2021-09-28 17:13:48 -0700

    Get Pal1 to work properly with proper formatting

--- src/main/java/com/example/sping_portfolio/Minilab/ChrisFib/Pal1.java
+++ src/main/java/com/example/sping_portfolio/Minilab/ChrisFib/Pal1.java
@@ -11,49 +11,62 @@ package com.example.sping_portfolio.Minilab.ChrisFib;
 
 
 public class Pal1 {
-    public static void main(String[] args) {
-//      String word = "civic";
-//		String word = "radar";
-//		String word = "level";
-		String word = "elephant";
-//		String word = "java";
+    // initialize instance variable
+    private static String word;
 
-        System.out.println("The Given String \"" + word + "\"");
-        if (checkPalindrome(word))
-            System.out.println("Yes, it is a Palindrome");
-        else
-            System.out.println("No, it is NOT a Palindrome");
+    // Constructor
+    public Pal1(String word)
+    {
+        this.word = word;
     }
 
-    public static boolean checkPalindrome(String str) {
-        int strLen = str.length();
+    //Getter
+    public static String getWord(){return word;}
 
+    //Method
+    public static boolean checkPalindrome(String word) {
+        int strLen = word.length();
         //The logic is compare 1st char with last char (nth char)
         //Then compare 2nd char with (n-1) char
         //here index starts with 0, so compare index 0 with index (n-1)
         //then compare index 1 with index (n-2)
         //do this comparison for half of the string,
         //if is palindrome 2nd half the string is mirror(reverse) of the 1st half
-        for (int i = 0; i < str.length() / 2; i++)
-            if (str.charAt(i) != str.charAt(strLen - i - 1))
+        for (int i = 0; i < word.length() / 2; i++)
+            if (word.charAt(i) != word.charAt(strLen - i - 1))
                 return false;
         return true;
     }
+
+    //runs program
+    public static void main(String[] args) {
+        //System.out.println("The Given String \"" + word + "\"");
+        //if (checkPalindrome(word))
+        //System.out.println("Yes, it is a Palindrome");
+        //else
+        //System.out.println("No, it is NOT a Palindrome");
+
+
+        Pal1 myPal = new Pal1("word");
+        System.out.println("The Given String \"" + word + "\"");
+        myPal.checkPalindrome(getWord());
+        myPal.getWord();
+        //System.out.print(checkPalindrome());
+    }
 }
 /*
-	OUTPUT
+   OUTPUT
 
-	The Given String "malayalam"
-	Yes, it is a Palindrome
+   The Given String "malayalam"
+   Yes, it is a Palindrome
 
-	The Given String "radar"
-	Yes, it is a Palindrome
-	The Given String "civic"
-	Yes, it is a Palindrome
+   The Given String "radar"
+   Yes, it is a Palindrome
+   The Given String "civic"
+   Yes, it is a Palindrome
 
-	The Given String "elephant"
-	No, it is NOT a Palindrome
-	The Given String "java"
-	No, it is NOT a Palindrome
+   The Given String "elephant"
+   No, it is NOT a Palindrome
+   The Given String "java"
+   No, it is NOT a Palindrome
 */
-
--- src/main/java/com/example/sping_portfolio/Minilab/ChrisFib/TPT5.java
+++ src/main/java/com/example/sping_portfolio/Minilab/ChrisFib/TPT5.java
@@ -4,8 +4,6 @@ import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import com.example.sping_portfolio.Minilab.ChrisFib.Pal1;
-import com.example.sping_portfolio.Minilab.ChrisFib.Pal2;
 
 
 @Controller
@@ -13,16 +11,26 @@ public class TPT5 {
     @GetMapping("/palindrome1")
     //  For algorithm
     // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
-    public String Pal1(@RequestParam(name = "word", required = false, defaultValue = "") String word, Model model) {
-        if (Pal1.checkPalindrome(word)) {
-            System.out.println(word + " palindrome");
-            // uses ${message} from inputted date on Pal1.html and prints the word with " is a palindrome"
-            model.addAttribute("message", word + " is a palindrome"); // MODEL is passed to html
-        } else {
-            System.out.println(word + " not a palindrome");
-            // uses ${message} from inputted data on Pal1.html and prints the word with " is not a palindrome"
-            model.addAttribute("message", word + " is not a palindrome");
+    public String Pal1(@RequestParam(name = "word", required = false, defaultValue = "Palindrome") String message, Model model) {
+        Pal1 myPal = new Pal1("word");
+        if (myPal.checkPalindrome(message)){
+            System.out.println(message + " is a palindrome");
+            model.addAttribute("message", message + " is a palindrome");
         }
+        else {
+            System.out.println(message + " is not a palindrome");
+            model.addAttribute("message", message + "is not a palindrome");
+        }
+
+        //if (Pal1.checkPalindrome()) {
+        //System.out.println(word + " palindrome");
+        // uses ${message} from inputted date on Pal1.html and prints the word with " is a palindrome"
+        //model.addAttribute("message", word + " is a palindrome"); // MODEL is passed to html
+        //} //else {
+        //System.out.println(word + " not a palindrome");
+        // uses ${message} from inputted data on Pal1.html and prints the word with " is not a palindrome"
+        //model.addAttribute("message", word + " is not a palindrome");
+        //}
 
         return "Minilab/ChrisTPT5/Pal1"; // returns HTML VIEW ()
     }
@@ -42,39 +50,5 @@ public class TPT5 {
         }
 
         return "Minilab/ChrisTPT5/Pal2"; // returns HTML VIEW ()
-    }
-
-    @GetMapping("/palindrome3")
-    //   algorithm
-    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
-    public String Pal3(@RequestParam(name = "word", required = false, defaultValue = "") String word, Model model) {
-        if (Pal2.isPalindrome(word)) {
-            System.out.println(word + " palindrome");
-            // uses ${message} from inputted date on Pal1.html and prints the word with " is a palindrome"
-            model.addAttribute("message", word + " is a palindrome"); // MODEL is passed to html
-        } else {
-            System.out.println(word + " not a palindrome");
-            // uses ${message} from inputted data on Pal1.html and prints the word with " is not a palindrome"
-            model.addAttribute("message", word + " is not a palindrome");
-        }
-
-        return "Minilab/ChrisTPT5/Pal3"; // returns HTML VIEW ()
-    }
-
-    @GetMapping("/palindrome4")
-    //   algorithm
-    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
-    public String Pal4(@RequestParam(name = "word", required = false, defaultValue = "") String word, Model model) {
-        if (Pal2.isPalindrome(word)) {
-            System.out.println(word + " palindrome");
-            // uses ${message} from inputted date on Pal1.html and prints the word with " is a palindrome"
-            model.addAttribute("message", word + " is a palindrome"); // MODEL is passed to html
-        } else {
-            System.out.println(word + " not a palindrome");
-            // uses ${message} from inputted data on Pal1.html and prints the word with " is not a palindrome"
-            model.addAttribute("message", word + " is not a palindrome");
-        }
-
-        return "Minilab/ChrisTPT5/Pal4"; // returns HTML VIEW ()
     }
 }
