d763f07a004f638959c0fc36e8428797c47826de
Chris-Ru <72892271+Chris-Ru@users.noreply.github.com>
2021-10-04 10:38:06 -0700

    help sam with factorial

---
+++ src/main/java/com/example/sping_portfolio/Minilab/Minilab4/Sam/fact.java
@@ -0,0 +1,62 @@
+package com.example.sping_portfolio.Minilab.Minilab4.Sam;
+
+/*
+ * Palindrome Check
+ * -----------------
+ * A String is a palindrome, if it is unchanged even when it is reversed.
+ *
+ * example words are :
+ * radar, civic, level, racecar
+ */
+
+public class fact {
+    // initialize instance variable
+    private static int number;
+
+    // Constructor
+    public fact(int number)
+    {
+        this.number = number;
+    }
+
+    // Getter
+    public static int getNumber(){return number;}
+
+    // Method
+    public static boolean checkNumber(int number) {
+
+    }
+
+    //runs program
+    public static void main(String[] args) {
+        //System.out.println("The Given String \"" + word + "\"");
+        //if (checkPalindrome(word))
+        //System.out.println("Yes, it is a Palindrome");
+        //else
+        //System.out.println("No, it is NOT a Palindrome");
+
+
+//        com.example.sping_portfolio.Minilab.ChrisFib.nonabstract.Pal1 myPal = new com.example.sping_portfolio.Minilab.ChrisFib.nonabstract.Pal1("civic");
+//        System.out.println("The Given String \"" + word + "\"");
+//        myPal.checkPalindrome(getWord());
+//        myPal.getWord();
+//        System.out.print(checkPalindrome(word));
+    }
+}
+
+/*
+   OUTPUT
+
+   The Given String "malayalam"
+   Yes, it is a Palindrome
+
+   The Given String "radar"
+   Yes, it is a Palindrome
+   The Given String "civic"
+   Yes, it is a Palindrome
+
+   The Given String "elephant"
+   No, it is NOT a Palindrome
+   The Given String "java"
+   No, it is NOT a Palindrome
+*/
--- src/main/java/com/example/sping_portfolio/Minilab/Minilab4/Sam/factorial.java
+++ src/main/java/com/example/sping_portfolio/Minilab/Minilab4/Sam/factorial.java
@@ -1,7 +1,5 @@
 package com.example.sping_portfolio.Minilab.Minilab4.Sam;
 
-import com.example.sping_portfolio.Minilab.Minilab4.Sam.factorial;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,15 +8,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class factorial {
     @GetMapping("/factorial")
-        public String fac(@RequestParam(name="number", defaultValue="", required = false)int number, Model model){  
-        int i,fact=1;  
-        for(i=1;i<=number;i++){    
-            fact=fact*i;    
-        }    
+    public String fac(@RequestParam(name = "name", required = false, defaultValue = "World") int number, Model model) {
+        int i,fact=1;
+        for(i=1;i<=number;i++){
+            fact=fact*i;
+        }
         model.addAttribute("number", number);
         model.addAttribute("fact", fact);
-        System.out.println("Factorial of "+number+" is: "+fact);    
-        return "Minilab/factorial"; // returns HTML VIEW ()
-        } 
+        System.out.println("Factorial of "+number+" is: "+fact);
+        return "Minilab/factorial"; // returns HTML VIEW (greeting)
     }
+}
 
