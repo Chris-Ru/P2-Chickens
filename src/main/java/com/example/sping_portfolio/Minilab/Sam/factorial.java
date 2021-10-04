bd97ddf822489353e45def8ed2e768efd98c462d
samkoenig9 <samkoenig100@gmail.com>
2021-10-04 10:08:37 -0700

    factorial progress

---
+++ src/main/java/com/example/sping_portfolio/Minilab/Sam/factorial.java
@@ -0,0 +1,23 @@
+package com.example.sping_portfolio.Minilab.Sam;
+
+import com.example.sping_portfolio.Minilab.Sam.factorial;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class factorial {
+    @GetMapping("/factorial")
+        public String fac(@RequestParam(name="number", defaultValue="", required = false)int number, Model model){  
+        int i,fact=1;  
+        for(i=1;i<=number;i++){    
+            fact=fact*i;    
+        }    
+        model.addAttribute("number", number);
+        model.addAttribute("fact", fact);
+        System.out.println("Factorial of "+number+" is: "+fact);    
+        return "Minilab/factorial"; // returns HTML VIEW ()
+        } 
+    }
+
