742401ad670a61d1f8e7c6b65129d320833ead2d
dylanroman <dylanroman143@gmail.com>
2021-11-04 22:54:46 -0700

    Revert "added hibernate, created connection between java and sqlite"
    This reverts commit c963c293f4745ff2d34ef3c1c3b2596fbf6167e3.

--- src/main/java/com/example/sping_portfolio/functions/database.java
+++ src/main/java/com/example/sping_portfolio/functions/database.java
@@ -9,16 +9,15 @@ import java.sql.Statement;
 public class database {
 
     public static void main(String[] args) throws SQLException {
-        String jdbcURL = "jdbc:sqlite:/sqlite.db";
+        String jdbcURL = "jdbc:sqlite:/newsrating.db";
         Connection connection = DriverManager.getConnection(jdbcURL);
 
         //create table
         String sql = "create table users (id varchar(4), username varchar(15), password varchar(20))";
         Statement statement = connection.createStatement();
-        statement.executeUpdate(sql);
+        statement.execute(sql);
 
         System.out.println("Table Created");
-
     }
 
 }
