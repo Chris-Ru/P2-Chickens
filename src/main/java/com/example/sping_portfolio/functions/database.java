c963c293f4745ff2d34ef3c1c3b2596fbf6167e3
dylanroman <dylanroman143@gmail.com>
2021-11-04 22:32:35 -0700

    added hibernate, created connection between java and sqlite

--- src/main/java/com/example/sping_portfolio/functions/database.java
+++ src/main/java/com/example/sping_portfolio/functions/database.java
@@ -9,15 +9,16 @@ import java.sql.Statement;
 public class database {
 
     public static void main(String[] args) throws SQLException {
-        String jdbcURL = "jdbc:sqlite:/newsrating.db";
+        String jdbcURL = "jdbc:sqlite:/sqlite.db";
         Connection connection = DriverManager.getConnection(jdbcURL);
 
         //create table
         String sql = "create table users (id varchar(4), username varchar(15), password varchar(20))";
         Statement statement = connection.createStatement();
-        statement.execute(sql);
+        statement.executeUpdate(sql);
 
         System.out.println("Table Created");
+
     }
 
 }
